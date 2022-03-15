f5a67b2c5b7476e4cecadd390cad39d60f73b629
Justin Swift <jlswift2@gmail.com>
2022-03-15 10:54:01 -0500

    commit tree ad53670bd717a4fb2296c716b3b8923dad32a894

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,25 @@
 require 'pry'
 
 class String
+  
+  def report_on_self
+    "Self is: #{self}"
+  end
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    sentences = self.split(/\.|\?|!/).select {|string| !string.empty?}
+    sentences.length
   end
 end
